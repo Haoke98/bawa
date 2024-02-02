93d8a38d07228c8ca90329024177ba9ae2e12542
Sadam·Sadik <1903249375@qq.com>
2024-02-02 10:50:31 +0800

    初始化代码

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # bawa
-A data visualize editor on Web.
+A data visualize editor on Web. It is the source code of online visual chart design (https://sdc.mldoo.com).
---
+++ src/js/api.js
@@ -0,0 +1,1 @@
+(function(){ window.BMap_loadScriptTime = (new Date).getTime(); document.write('<script type="text/javascript" src="http://api.map.baidu.com/getscript?v=2.0&ak=BGBn4IcL3n0x5AFNrjQNeRld0Ursdug3&services=&t=20231026185850"></script>');})();
